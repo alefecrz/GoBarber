c5a538b7bcd05e9ce834e9dce04bb5a300726356
Alefe Cruz <alefecrz@gmail.com>
2020-12-21 21:59:04 -0300

    Create global style

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -1,20 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
+import { COLORS } from './themes/gobarber';
 
 export default createGlobalStyle`
-:root {
-  --color-primary: #FF9000;
-  --color-secundary: #3E3B47;
-  --color-background: #312E38;
-  --color-text-lighter: #F4EDE8;
-  --color-text-light:  #999591;
-  --color-text-dark: #666360;
-  --color-button-text: #312E38;
-  --color-input-background: #232129;
-  --color-box-header: #28262E;
-
-    font-size: 100%;
-  }
-
   * {
     margin: 0;
     padding: 0;
@@ -22,20 +9,10 @@ export default createGlobalStyle`
     outline: 0;
   }
 
-  html, body, #root {
-    height: 100vh;
-  }
-
   body {
-    background: var(--color-background);
-    color: var(--color-text-lighter);
+    background: ${COLORS.background};
+    color: ${COLORS.textLighter};
     -webkit-font-smoothing: antialiased;
-  }
-
-  #root {
-    display: flex;
-    align-items: center;
-    justify-content: center;
   }
 
   body,
@@ -43,19 +20,14 @@ export default createGlobalStyle`
   button,
   textarea {
     font-family: 'Roboto Slab', serif;
-    font: 1.6rem;
+    font-size: 16px;
   }
 
   h1, h2, h3, h4, h5, strong {
     font-weight:500;
   }
+
   button {
     cursor: pointer;
-  }
-
-  @media (min-width: 700px) {
-    :root {
-      font-size: 62.5%;
-    }
   }
 `;
---
+++ web/src/styles/themes/gobarber.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#FF9000',
+  secundary: '#3E3B47',
+  background: '#312E38',
+  textLighter: '#F4EDE8',
+  textLight: '#999591',
+  textDark: '#666360',
+  buttonText: '#312E38',
+  inputBackground: '#232129',
+  boxHeader: '#28262E',
+};
